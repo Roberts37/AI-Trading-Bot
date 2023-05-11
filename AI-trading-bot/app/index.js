72b7a81fd4a6454f784e5e9c84340250b759f91e
Trevor Larkin <106640268+tlarkin011@users.noreply.github.com>
2023-05-11 13:48:23 -0700

    added styles

--- AI-trading-bot/app/index.js
+++ AI-trading-bot/app/index.js
@@ -1,27 +1,58 @@
 import { useState } from 'react';
-import {View, Text, SafeAreaView, ScrollView } from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet } from 'react-native';
 import {Stack, useRouter } from 'expo-router';
 
-import { COLORS, icons, images, SIZES } from '../constants';
 
 
-const Home = () => {
-    const router = useRouter();
+export default function App() {
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
-            <Stack.Screen
-            options={{
-                headerStyle: { backgroundColor: COLORS.lightWhite},
-                headerShadowVisible: false,
-                headerLeft: () => (
-                    <ScreenHeaderBtn iconUrl={icons.menu} demension="60%" />
-                ),
-                headerRight: () => (
-                    <ScreenHeaderBtn iconUrl={icons.profile} demension="60%" />
-                )
-            }} />  
-        </SafeAreaView>
-    )
-}
-
-export default Home;
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Hal's hacking Machine</Text>
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.text}>Trading Time!</Text>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.text}>© 2023 My Finance App. All rights reserved.</Text>
+        </View>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    header: {
+      height: 100,
+      backgroundColor: '#007AFF',
+      justifyContent: 'center',
+      alignItems: 'center',
+      
+    },
+    title: {
+      color: '#fff',
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    content: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    footer: {
+      height: 30,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderTopWidth: 1,
+      borderTopColor: '#ddd',
+    },
+  });
